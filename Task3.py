fc15b40287211fd5d985aac1d143a68a5f64c349
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-05-20 19:55:26 +0600

    Task3 update

--- Task3.py
+++ Task3.py
@@ -5,15 +5,19 @@
 # Input: 20 21 22(ввод чисел НЕ в одну строку)
 # Output: 32
 
+def get_count_desk(count_student_class, count_students_desk):
+    return (count_student_class + count_students_desk - 1) // count_students_desk
+
+
 class1 = int(input('Введите количество учащихся в классе 1: '))
 class2 = int(input('Введите количество учащихся в классе 2: '))
 class3 = int(input('Введите количество учащихся в классе 3: '))
 
 count_desk = 0
-count_students = 2
+count_students_desk = 2
 
-count_desk += (class1 + count_students - 1) // count_students
-count_desk += (class2 + count_students - 1) // count_students
-count_desk += (class3 + count_students - 1) // count_students
+count_desk += get_count_desk(class1, count_students_desk)
+count_desk += get_count_desk(class2, count_students_desk)
+count_desk += get_count_desk(class3, count_students_desk)
 
 print(count_desk)
